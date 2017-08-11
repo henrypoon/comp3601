f999c6b3fa43bb9c689681f187732217fd439c2b
Luong Ong <z5020097@zmail.unsw.edu.au>
2017-08-11 15:46:46 +1000

    adapted to musicplayer needs testing

--- testbench.vhd
+++ testbench.vhd
@@ -9,19 +9,19 @@ USE STD.TEXTIO.ALL;
 ENTITY testbench IS
 END testbench;
 ARCHITECTURE testbench_arch OF testbench IS
-  FILE RESULTS: TEXT OPEN WRITE_MODE IS "results.txt";
-  COMPONENT multiprocessor
+ -- FILE RESULTS: TEXT OPEN WRITE_MODE IS "results.txt";
+  COMPONENT musicplayer
     PORT (
-          reset : In std_logic;
-          clk : In std_logic;
-          ascii1 : in std_logic_vector(6 downto 0);
-          ascii2 : in std_logic_vector(6 downto 0);
-          ready1 : out std_logic;
-          ready2 : out std_logic;
-          stall_p1 : out std_logic;
-          stall_p2 : out std_logic
+        reset     : in std_logic;
+        clk       : in std_logic;
+       music_data : in std_logic_vector(11 downto 0); --??? Depends on the format of the text file input
+        sensor    : in std_logic;
+        ready     :out std_logic; --??? might not need
+        sound     :out std_logic --pwm output 
           );
     END COMPONENT;
+    
+    
 
   SIGNAL reset : std_logic := '1';
   SIGNAL clk : std_logic := '0';
@@ -34,31 +34,24 @@ ARCHITECTURE testbench_arch OF testbench IS
   -- file IO relevant declarations
   --------------------------------------------
   file stimuli_file : text open read_mode is "stimuli.txt";
-  file stimuli_file2 : text open read_mode is "stimuli2.txt";
-  file stimuli_file3 : text open read_mode is "stimuli3.txt";
-  file results_file : text open write_mode is "stimresults.txt";
-  --signal x : std_logic_vector(7 downto 0);
-  signal char : character;
-  signal char2: character;
-  signal char_vector : std_logic_vector(6 downto 0);
-  signal char_vector2 : std_logic_vector(6 downto 0);
+  --signal char : character;
+  signal char : std_logic_vector(11 downto 0);
+  --signal char_vector : std_logic_vector(6 downto 0);
+  signal char_vector : std_logic_vector(11 downto 0);
   signal ready_next_char : std_logic;
-  signal ready_next_char2 : std_logic;
-  signal sig_stall_p1 : std_logic;
-  signal sig_stall_p2 : std_logic;
-  
+  signal sig_sound :std_logic;
+  signal sig_sensor:std_logic;
   BEGIN
-    UUT : multiprocessor
+    UUT : musicplayer
         PORT MAP (
                   reset => reset,
                   clk => clk,
-                  ascii1 => char_vector,
-                  ascii2 => char_vector2,
-                  ready1 => ready_next_char,
-                  ready2 => ready_next_char2,
-                  stall_p1 => sig_stall_p1,
-                  stall_p2 => sig_stall_p2
+                  music_data => char_vector,
+                  sensor => sig_sensor,
+                  ready => ready_next_char,
+                  sound => sig_sound
                   );
+                  
   PROCESS -- clock process for clk
   BEGIN
     WAIT for OFFSET;
@@ -73,88 +66,46 @@ ARCHITECTURE testbench_arch OF testbench IS
   PROCESS
   BEGIN
   -- ------------- Current Time: 285ns
-  WAIT FOR 285 ns;
-  reset <= '0';
+    WAIT FOR 285 ns;
+    reset <= '0';
   -- -------------------------------------
-  WAIT FOR 280000 ns;
-  IF (TX_ERROR = 0) THEN
-  STD.TEXTIO.write(TX_OUT, string'("No errors or warnings"));
-  STD.TEXTIO.writeline(RESULTS, TX_OUT);
-  ASSERT (FALSE) REPORT
-  "Simulation successful (not a failure). No problems detected."
-  SEVERITY FAILURE;
-  ELSE
-  STD.TEXTIO.write(TX_OUT, TX_ERROR);
-  STD.TEXTIO.write(TX_OUT,
-  string'(" errors found in simulation"));
-  STD.TEXTIO.writeline(RESULTS, TX_OUT);
-  ASSERT (FALSE) REPORT "Errors found during simulation"
-  SEVERITY FAILURE;
-  END IF;
   END PROCESS;
+  
   -- Handle reading data from the pattern file and the input "stream" file
   stimuli_gen_fileio: process (clk, reset, ready_next_char)
-  variable data_buf : character;--std_logic_vector(7 downto 0);
+  --variable data_buf : character;--std_logic_vector(7 downto 0); --old code
+  variable data_buf : std_logic_vector(11 downto 0); --new code
   variable line_buf : line;
+  
   variable num_cycles : integer := 3;
-  variable pattern_length : integer := 64;
+  --variable pattern_length : integer := 64;
   variable buffer_length : integer := 64;
+  
   begin
-  -- wait for reset and clock signals
-  if reset = '1' then
-  char_vector <= (others => '0');
-  elsif rising_edge(clk) then
-  if (num_cycles = 3) then
-  -- read pattern (aka stimuli.txt)
-  if ((not endfile(stimuli_file)) and pattern_length > 0) then
-  -- read from file
-  readline(stimuli_file, line_buf);
-  --writeline(results_file, line_buf);
-  read(line_buf, data_buf);
-  --x <= data_buf;
-  char <= data_buf;
-  char_vector <= CONV_STD_LOGIC_VECTOR(character'pos(data_buf), 7);
-  --wait for 1 ns; -- wait awhile for signals of the DUT to update
-  --hwrite(line_buf, x);
-  --writeline(results_file, line_buf);
-  pattern_length := pattern_length - 1;
-  elsif (pattern_length >= 0) then
-  char_vector <= (others => '0');
-  pattern_length := pattern_length - 1;
-  -- read stream aka stimuli2.txt
-  elsif ((not endfile(stimuli_file2)) and (buffer_length > 0)) then
-  -- read from file
-  readline(stimuli_file2, line_buf);
-  read(line_buf, data_buf);
-  --x <= data_buf;
-  char_vector <= CONV_STD_LOGIC_VECTOR(character'pos(data_buf), 7);
-  --wait for 1 ns; -- wait awhile for signals of the DUT to update
-  --hwrite(line_buf, x);
-  --writeline(results_file, line_buf);
-  buffer_length := buffer_length - 1;
-  elsif (buffer_length > 0) then
-  char_vector <= (others => '0');
-  buffer_length := buffer_length - 1;
-  end if;
-  num_cycles := 0;
-  elsif (sig_stall_p1 = '0') then
-  num_cycles := num_cycles + 1;
-  if (num_cycles > 3) then
-  num_cycles := 3;
-  end if;
-  end if;
-  if (ready_next_char = '1') then
-  --std.textio.write(TX_OUT, string'("Push new char here?"));
-  --STD.TEXTIO.writeline(RESULTS, TX_OUT);
-  if (not endfile(stimuli_file2)) then
-  readline(stimuli_file2, line_buf);
-  read(line_buf, data_buf);
-  char_vector <= CONV_STD_LOGIC_VECTOR(character'pos(data_buf), 7);
-  else
-  char_vector <= (others => '0');
-  end if;
-  end if;
-  end if;
+    -- wait for reset and clock signals
+    if reset = '1' then
+      char_vector <= (others => '0');
+    elsif rising_edge(clk) then
+      if (num_cycles = 3) then
+        -- read pattern (aka stimuli.txt)
+          --if ((not endfile(stimuli_file)) and pattern_length > 0) then
+          if (not endfile(stimuli_file)) then
+            -- read from file
+            readline(stimuli_file, line_buf);
+            --writeline(results_file, line_buf);
+            read(line_buf, data_buf);
+            char <= data_buf;
+            --char_vector <= CONV_STD_LOGIC_VECTOR(character'pos(data_buf), 7);
+            char_vector <= data_buf;
+            
+            --wait for 1 ns; -- wait awhile for signals of the DUT to update
+            --hwrite(line_buf, x);
+            --writeline(results_file, line_buf);
+          else
+            char_vector <= (others => '0');                
+         end if; 
+      end if;
+    end if;
   end process;
-  -- Handle reading data from the pattern file and the input "stream" file
+
 END testbench_arch;
