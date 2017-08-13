522d1605de0317f96fe8f0ced555a5b6ab70860e
Angelo Yan <z5025212@student.unsw.edu.au>
2017-08-14 09:51:17 +1000

    first 8 bits (notes) finished

--- converter.py
+++ converter.py
@@ -2,54 +2,62 @@ fin = open('music.txt', 'r')
 fout = open('binary.txt', 'w')
 result = ''
 
-# store text file in a list
-lines = []
-for line in fin:
-    # ignore leading and trailing whitespace
-    # case insensitive
-    line = line.strip().lower()
-    # ignore empty lines
-    if line:
-        # ignore comments
-        if '%' in line:
-            line = list(line)
-            index = line.index('%')
-            line = ''.join(line)
-            line = line[0:index]
-        lines.append(line)
-
 # dictionary of notes
 # 17 notes per octave
-# 4 octaves required
-# 68 notes in required range
-# rest
-# total 69 items
+# only 12 frequencies
+# 3 octaves required and 1 note
+# 52 notes in required range
+# 37 frequencies in required range
+# dictionary requires 38 entries to include rest
 binary = {}
 
 binary['rest'] = 0
 
 count = 1
+octave = 3
 
 #range is actually A3 to G6
-for octave in range(3, 7):
-    for letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g']:
+while count <= 38:
+    for letter in ['c', 'd', 'e', 'f', 'g', 'a', 'b']:
         # flat
         # C and F do not have flats
-        if letter != 'c' or letter != 'f':
-            binary[letter + str(octave) + '#'] = count
+        if letter != 'c' and letter != 'f':
+            binary[letter + str(octave) + 'b'] = format(count, '06b')
             count += 1
-
+        
         # normal
-        binary[letter + str(octave)] = count
+        binary[letter + str(octave)] = format(count, '06b')
         count += 1
 
         # sharp
         # B and E do not have sharps
-        binary[letter + str(octave) + 'b'] = count
-        count += 1
+        if letter != 'b' and letter != 'e':
+            binary[letter + str(octave) + '#'] = format(count, '06b')
 
-#BPM and timing between notes
-for line in lines:
-    print(line)
+        if letter == 'g':
+            octave += 1
+
+binary['c6'] = format(count, '06b')
 
 print(binary)
+
+#BPM and timing between notes
+
+
+# store text file in a list
+for line in fin:
+    # ignore leading and trailing whitespace
+    # case insensitive
+    line = line.strip().lower()
+    # ignore comments
+    if '%' in line:
+        line = list(line)
+        index = line.index('%')
+        line = ''.join(line)
+        line = line[0:index]
+        line = line.strip()
+    # ignore empty lines
+    if line:
+        result += binary[line]
+
+print(result)
