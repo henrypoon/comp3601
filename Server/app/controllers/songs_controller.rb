b7c235a07bee9170cd660232bf174a69b1b01fee
Henry Pan <henrypoon1231@gmail.com>
2017-09-08 00:48:20 +1000

    create & destroy

--- Server/app/controllers/songs_controller.rb
+++ Server/app/controllers/songs_controller.rb
@@ -7,4 +7,29 @@ class SongsController < ApplicationController
 	def index
 		render json: Song.all
 	end
+
+	def create
+		@song = Song.new(song_params)
+		if @song.save
+			render json: @song
+		else
+			render json: {status: 'ERROR'}
+		end
+	end
+
+	def destroy
+		@song = Song.find(params[:id])
+		@song.destroy
+	end
+
+
+	def play
+		system('echo fuck')
+		render json: Song.all
+	end
+
+	private
+	def song_params
+		params.permit(:notes, :mode, :bpm)
+	end
 end
--- Server/config/routes.rb
+++ Server/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :songs, only: [:index, :create, :show, :update, :destroy, :play]
+  resources :songs, only: [:index, :create, :show, :update, :destroy] do
+  	collection do
+  		get 'play'
+  	end
+  end
 end
