f4d3362641b1b52e619760ce6ba540bc70a5e212
Henry Pan <henrypoon1231@gmail.com>
2017-09-10 14:45:32 +1000

    minor change

--- Server/config/routes.rb
+++ Server/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :songs, only: [:index, :create, :show, :update, :destroy] do
   	collection do
-  		get '/play/:id', to: "songs#play"
+  		post '/play/:id', to: "songs#play"
   	end
   end
 end
