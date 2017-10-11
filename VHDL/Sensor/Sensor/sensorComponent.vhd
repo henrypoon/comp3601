f6c568425417d56d91d34dc3c8f30c5eeb7fe524
AIkra <akmal9000@gmail.com>
2017-10-11 20:48:28 +1100

    Add files via upload

--- VHDL/Sensor/Sensor/sensorComponent.vhd
+++ VHDL/Sensor/Sensor/sensorComponent.vhd
@@ -34,60 +34,45 @@ entity sensorComponent is
            pulse : in  STD_LOGIC;
            timerSwitch : in  STD_LOGIC;
            trig : out  STD_LOGIC;
+			  rst: in std_logic;
            swingValue : out  STD_LOGIC_VECTOR (7 downto 0));
 end sensorComponent;
 
-architecture Behavioral of sensorComponent is
+architecture Behavioral of sensorComponent is 
 
-component timer is
-    Port ( clk: in std_logic;
-			  en: in std_logic;
-			  rst: in std_logic;
+component trySense is
+    Port ( clk : in  STD_LOGIC;
+           trig : out  STD_LOGIC;
+           echo : in  STD_LOGIC;
+           myOut : out  STD_LOGIC);
+end component;
+
+component timerMod is
+    Port ( clk : in  STD_LOGIC;
+           en : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
            done : out  STD_LOGIC);
 end component;
 
-component sensoModule is
+component swingmodule is
     Port ( clk : in  STD_LOGIC;
-           pulse : in  STD_LOGIC;
-			  trig: out std_logic;
-           senseOut: out std_logic);
-end component;
-
-component multiplier is
-    Port ( a : in  STD_LOGIC_VECTOR (7 downto 0);
-           res : out  STD_LOGIC_VECTOR (7 downto 0));
-end component;
-
-component swingCounter is
-    Port ( clk : in  STD_LOGIC;
+           en : in  STD_LOGIC;
            rst : in  STD_LOGIC;
-			  done: in std_logic;
-           swingNum : out  STD_LOGIC_VECTOR (7 downto 0));
+			  release: in std_logic;
+           swingout : out  STD_LOGIC_VECTOR (7 downto 0));
 end component;
 
-signal timerDone: std_logic;
-signal sensorOutput: std_logic;
-signal swings: std_logic_vector(7 downto 0);
-signal totalSwings: std_logic_vector(7 downto 0);
-signal toOut: std_logic_vector(7 downto 0);
+signal clkSensor: std_logic;
+signal swingSwitch: std_logic;
+signal outSwing: std_logic_vector(7 downto 0);
 
 begin
 
-timr: timer port map(clk, timerSwitch, timerSwitch, timerDone);
-sensModule: sensoModule port map(clk, pulse, trig, sensorOutput);
-swingCtr: swingCounter port map(sensorOutput, timerSwitch, timerDone, swings);
-mult: multiplier port map(swings, totalSwings); 
+sensor: trySense port map(clk, trig, pulse, clkSensor);
+swingTimer: timerMod port map(clk, timerSwitch, rst, swingSwitch);
+swingComp: swingmodule port map(clkSensor, timerSwitch, rst, swingSwitch, outSwing);
 
-process(timerSwitch, toOut, timerDone)
-begin
-	if timerSwitch = '0' then
-		toOut <= (others=>'0');
-	elsif timerDone = '1' and toOut = "00000000" then
-		toOut <= totalSwings;
-	end if;
-end process;
-
-swingValue <= toOut;
+swingValue <= outSwing;
 
 end Behavioral;
 
